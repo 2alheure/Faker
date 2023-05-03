68102e5e3d873888f508d97ce1d18ae1d4a80ec6
2alheure <jojo77.juv@live.fr>
2023-05-03 20:06:50 +0200

    Adding auto guess for "name" + "telephone" + "url"

--- src/Faker/Guesser/Name.php
+++ src/Faker/Guesser/Name.php
@@ -4,25 +4,14 @@ namespace Faker\Guesser;
 
 use \Faker\Provider\Base;
 
-class Name
-{
+class Name {
     protected $generator;
 
-    /**
-     * @param \Faker\Generator $generator
-     */
-    public function __construct(\Faker\Generator $generator)
-    {
+    public function __construct(\Faker\Generator $generator) {
         $this->generator = $generator;
     }
 
-    /**
-     * @param string $name
-     * @param int|null $size Length of field, if known
-     * @return callable
-     */
-    public function guessFormat($name, $size = null)
-    {
+    public function guessFormat($name) {
         $name = Base::toLower($name);
         $generator = $this->generator;
         if (preg_match('/^is[_A-Z]/', $name)) {
@@ -35,14 +24,20 @@ class Name
                 return $generator->dateTime;
             };
         }
-        switch (str_replace('_', '', $name)) {
+        switch ($name) {
+            case 'first_name':
             case 'firstname':
                 return function () use ($generator) {
                     return $generator->firstName;
                 };
+            case 'last_name':
             case 'lastname':
                 return function () use ($generator) {
                     return $generator->lastName;
+                };
+            case 'name':
+                return function () use ($generator) {
+                    return $generator->name;
                 };
             case 'username':
             case 'login':
@@ -50,14 +45,12 @@ class Name
                     return $generator->userName;
                 };
             case 'email':
-            case 'emailaddress':
                 return function () use ($generator) {
                     return $generator->email;
                 };
+            case 'phone_number':
             case 'phonenumber':
             case 'phone':
-            case 'telephone':
-            case 'telnumber':
                 return function () use ($generator) {
                     return $generator->phoneNumber;
                 };
@@ -66,7 +59,6 @@ class Name
                     return $generator->address;
                 };
             case 'city':
-            case 'town':
                 return function () use ($generator) {
                     return $generator->city;
                 };
@@ -83,73 +75,22 @@ class Name
                 return function () use ($generator) {
                     return $generator->state;
                 };
-            case 'county':
-                if ($this->generator->locale == 'en_US') {
-                    return function () use ($generator) {
-                        return sprintf('%s County', $generator->city);
-                    };
-                }
-
-                return function () use ($generator) {
-                    return $generator->state;
-                };
             case 'country':
-                switch ($size) {
-                    case 2:
-                        return function () use ($generator) {
-                            return $generator->countryCode;
-                        };
-                    case 3:
-                        return function () use ($generator) {
-                            return $generator->countryISOAlpha3;
-                        };
-                    case 5:
-                    case 6:
-                        return function () use ($generator) {
-                            return $generator->locale;
-                        };
-                    default:
-                        return function () use ($generator) {
-                            return $generator->country;
-                        };
-                }
-                break;
-            case 'locale':
                 return function () use ($generator) {
-                    return $generator->locale;
-                };
-            case 'currency':
-            case 'currencycode':
-                return function () use ($generator) {
-                    return $generator->currencyCode;
-                };
-            case 'url':
-            case 'website':
-                return function () use ($generator) {
-                    return $generator->url;
-                };
-            case 'company':
-            case 'companyname':
-            case 'employer':
-                return function () use ($generator) {
-                    return $generator->company;
+                    return $generator->country;
                 };
             case 'title':
-                if ($size !== null && $size <= 10) {
-                    return function () use ($generator) {
-                        return $generator->title;
-                    };
-                }
-
                 return function () use ($generator) {
                     return $generator->sentence;
                 };
             case 'body':
             case 'summary':
-            case 'article':
-            case 'description':
                 return function () use ($generator) {
                     return $generator->text;
+                };
+            case 'company':
+                return function () use ($generator) {
+                    return $generator->company;
                 };
         }
     }
