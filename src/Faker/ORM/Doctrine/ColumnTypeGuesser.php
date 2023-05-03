62706d32a55b47c09c29bb995835564a87fee5bd
2alheure <jojo77.juv@live.fr>
2023-05-03 20:06:59 +0200

    Changing namespaces for Doctrine 2

--- src/Faker/ORM/Doctrine/ColumnTypeGuesser.php
+++ src/Faker/ORM/Doctrine/ColumnTypeGuesser.php
@@ -2,24 +2,17 @@
 
 namespace Faker\ORM\Doctrine;
 
-use Doctrine\Common\Persistence\Mapping\ClassMetadata;
+use Doctrine\ORM\Mapping\ClassMetadata;
 
 class ColumnTypeGuesser
 {
     protected $generator;
 
-    /**
-     * @param \Faker\Generator $generator
-     */
     public function __construct(\Faker\Generator $generator)
     {
         $this->generator = $generator;
     }
 
-    /**
-     * @param ClassMetadata $class
-     * @return \Closure|null
-     */
     public function guessFormat($fieldName, ClassMetadata $class)
     {
         $generator = $this->generator;
@@ -66,12 +59,6 @@ class ColumnTypeGuesser
             case 'time':
                 return function () use ($generator) {
                     return $generator->datetime;
-                };
-            case 'datetime_immutable':
-            case 'date_immutable':
-            case 'time_immutable':
-                return function () use ($generator) {
-                    return \DateTimeImmutable::createFromMutable($generator->datetime);
                 };
             default:
                 // no smart way to guess what the user expects here
